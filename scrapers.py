997e996b6a82ad2ab955230c1b2e883004769537
0xA7404A <nyaurora@disroot.org>
2021-08-07 22:17:49 +0200

    C000de

---
+++ scrapers.py
@@ -0,0 +1,85 @@
+from requests import post, get
+from json import loads
+from tqdm import tqdm
+
+def autocomplete(region):
+    r = post(
+        'https://www.olx.pl/ajax/geo6/autosuggest/',
+        data = {
+            'data': region
+        }
+    )
+    return loads(r.text)['data']
+
+
+from requests import get
+from bs4 import BeautifulSoup as bs
+
+def get_pages(city_id):
+    r = bs(post(
+        'https://www.olx.pl/oferty/',
+        data = {
+            'search[city_id]': city_id
+        }
+    ).text, features='html.parser')
+
+    pages = []
+    for page in r.find('div', {'class': 'pager'}).find_all('a')[:-1]:
+        pages.append(page['href'])
+    pages.append(pages[0].split('?')[0])
+
+    return pages
+
+def scrap_offers_urls(pages):
+    offers = []
+    for page in pages:
+        r = bs(get(
+            page
+        ).text, features='html.parser')
+
+        for offer in r.find_all('div', {'class': 'offer-wrapper'}):
+            url = offer.find('a')['href']
+            if 'olx.pl/d/oferta' in url:
+                offers.append(url)
+
+    return offers
+
+def scrap_offers(offers_urls):
+    sellers = []
+
+    for i in tqdm(range(len(offers_urls)), desc='Scraping..'):
+        r = bs(get(
+            offers_urls[i]
+        ).text, features='html.parser')
+        card = r.find('div', {'data-cy': 'seller_card'})
+
+        seller = {
+            'name': card.find('h2').text,
+            'offers': [
+                offers_urls[i]
+            ],
+            'fb_connected': False,
+            'avatar': None,
+            'last_online': card.find('span').text.replace('Ostatnio online ', '')
+        }
+
+        if card.find('a'):
+            seller['url'] = 'https://www.olx.pl' + card.find('a')['href']
+        else:
+            seller['url'] = None
+
+        for img in card.find_all('img'):
+            if 'Facebook' in img['alt']:
+                seller['fb_connected'] = True
+            elif 'avatar' in img['alt']:
+                seller['avatar'] = img['src']
+
+        seller['registered_since'] = (
+            [i for i in card.find_all('div') if 'Na OLX od' in i.text][-1]
+            .text
+            .replace('Na OLX od ', '')
+        )
+
+        sellers.append(seller)
+
+    return sellers
---
+++ tool.py
@@ -0,0 +1,48 @@
+from scrapers import *
+from utils import *
+from json import dumps
+from time import time
+
+def get_region():
+    while True:
+        region_to_search = out('?', 'Region to search')
+        regions_found = autocomplete(region_to_search)
+
+        if len(regions_found):
+            out('I', 'Found regions')
+
+            i = 0
+            for region in regions_found:
+                out('I', region['text'], 4, i)
+                i += 1
+            out('I', 'Search another region', 4, 'S')
+
+            region_to_search = out('?', 'Pick a region from above')
+            try:
+                if int(region_to_search) in range(i):
+                    region_id = regions_found[int(region_to_search)]['id']
+                    break
+                else:
+                    out('E', 'Region outside of range')
+            except:
+                if region_to_search != 'S':
+                    out('E', 'Unknown region')
+        else:
+            out('W', 'Region not found!', 4)
+    return region_id
+
+if __name__ == '__main__':
+    region_id = get_region()
+    pages = get_pages(region_id)
+    out('I', 'Found ' + str(len(pages)) + ' pages of results')
+    if out('?', 'Do you want to proceed? (y/n)') not in ['y', 'Y']:
+        exit()
+    offers_urls = scrap_offers_urls(pages)
+    out('I', 'Found ' + str(len(offers_urls)) + ' offers')
+    sellers = scrap_offers(offers_urls)
+    out('I', 'Found ' + str(len(sellers)) + ' sellers')
+
+    file_path = 'olx_sellers_' + str(time()).split('.')[0] + '.json'
+    with open(file_path, 'a') as file:
+        file.write(dumps(sellers))
+    out('I', 'Exported sellers info to the ' + file_path + ' file')
